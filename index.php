c68dc86bf67210f2e8ef24a2abc1cfe371b869b3
ajay <silambu.ajayk@gmail.com>
2021-02-17 22:40:41 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Docker Assignment</title>
+
+      <!-- Latest compiled and minified Bootstrap CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+</head>
+
+<body>
+
+      <!-- container -->
+      <div class="container">
+
+            <div class="page-header">
+                  <h1>Docker Assignment</h1>
+            </div>
+
+            <!-- html form to create product will be here -->
+
+      </div> <!-- end .container -->
+
+      <div class="container">
+
+            <a class='btn btn-primary'>Add New Student Details</a>
+            <a class='btn btn-danger'>Read All Student Details</a>
+
+      </div>
+
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+      <!-- Latest compiled and minified Bootstrap JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
