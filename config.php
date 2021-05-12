1dada12ddb85a3f3dc0596a60449a24d94cd2399
Ajay-Chidambaram <56651112+Ajay-Chidambaram@users.noreply.github.com>
2021-05-12 22:52:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $json = file_get_contents('/var/www/html/credentials.json');
 
 $json_data = json_decode($json,true);
 
-$mysql_server_name = $json_data["SERVER"];
-$mysql_user_name = $json_data["USER_NAME"];
-$mysql_password = $json_data["PASSWORD"];
-$mysql_db_name = $json_data["DB_NAME"];
+$mysql_server_name = $json_data["SecretString"]["SERVER"];
+$mysql_user_name = $json_data["SecretString"]["USER_NAME"];
+$mysql_password = $json_data["SecretString"]["PASSWORD"];
+$mysql_db_name = $json_data["SecretString"]["DB_NAME"];
 
 $connection = new mysqli($mysql_server_name, $mysql_user_name, $mysql_password, $mysql_db_name);
 
